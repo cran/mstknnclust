60f9c97d7c210f3ed11d31b67b8da62b4f27069c
Jorge Parraga-Alava <jorge.parraga@usach.cl>
2019-02-06 14:50:09 +0000

    version 0.1.0

---
+++ R/dslanguages.R
@@ -0,0 +1,17 @@
+#' Indo-European languages dataset of class 'distance matrix'
+#'
+#' @description It contains the distances between 84 Indo-European languages based on the mean percent difference in cognacy, using the 200 Swadesh words.
+#'
+#' @docType data
+#'
+#' @usage data(dslanguages)
+#'
+#' @details Once the data set is loaded, it can be accessed as an object of class dataframe called \code{dslanguages}.
+#'
+#' @format An data frame of type distance matrix with 84 rows and columns.
+#'
+#'
+#' @references Dyen, I., Kruskal, J., and Black, P. (1992). An indoeuropean classification: A lexicostatistical experiment. Transactions of the American Philosophical Society. 82, (5).
+#'
+"dslanguages"
+
